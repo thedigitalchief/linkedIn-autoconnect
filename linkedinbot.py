84efa7c62bbc98b98448925b76318fd8da360b89
Dylan Nguyen <dylannguyen@macbookpro.socal.rr.com>
2022-04-17 05:34:44 -0700

    revised start functions

--- linkedinbot.py
+++ linkedinbot.py
@@ -15,28 +15,36 @@ import parsel
 #System.setProperty("webdriver.chrome.driver","/Users/dylannguyen/Documents/Coding - Local/Projects/Automated LinkedIn Networking Bot/automated-linkedin-networking-bot")
 
 #setting parameters so Chrome and webpage detect botting
-options = webdriver.ChromeOptions()
-options.add_argument("--no-sandbox")
-options.add_argument("--headless")
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--disable-extensions")
 
 
-#defining variables
 
-url = "https://www.linkedin.com/"
-network_url = "https://linkedin.com/mynetwork/"
-driver = webdriver.Chrome()
-driver.get(url)
+#logging in
+
+
 
 
 #log in to the LinkedIn account
-def login_to_linkedin(driver):
-  username = driver.find_element_by_id("session_key")
-  username.send_keys("me@dylanhnguyen.com")
-  password = driver.find_element_bçy_id("session_password")
-  password.send_keys("<password>")
-  driver.find_element_by_class_name("sign-in-form__submit-button").click()
+def login(driver):
+  driver = webdriver.Chrome()
+  url = 'https://www.linkedin.com'
+  network_url =  "http://linkedin.com/mynetwork/"
+  driver.get(url)
+  driver.implicitly_wait(3)
+
+  username_field = driver.find_element_by_xpath("//input[@name='session_key']")
+  password_field = driver.find_element_by_xpath("//input[@name='session_password']")
+
+  username = input('Type your username: ')
+  password = input(f'Type password for {username}: ')
+
+  username_field.send_keys(username)
+  password_field.send_keys(password)
+  driver.implicitly_wait(2)
+
+  submit_button = driver.find_element_by_xpath("//button[@type='submit']")
+  submit_button.click()
+
+  login(driver, url, network_url)
 
 
 def goto_network_page(driver,network_url):
@@ -82,11 +90,15 @@ def accept_invitations_from_users(driver):
         driver.find_element_by_class_name("invitation-card__action-btn artdeco-button--secondary").click()
 
 
+
 #connecting the functions and methods
-def main(driver,url,network_url):
-  start_bot(driver)
-  login_to_linkedin(driver)
+def start_bot(driver, url, network_url):
+  driver.get(url)
+  login(driver)
   goto_network_page(driver,network_url)
   send_requests_to_users(driver)
   accept_invitations_from_users(driver)
 
+if __name__ == "__start_bot__":
+    start_bot()
+    
