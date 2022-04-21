d338103caf4df11942c30a9b49dff2c7a3b7e33b
Dylan Nguyen <dylannguyen@macbookpro.socal.rr.com>
2022-04-21 05:51:32 -0700

    pycache update

--- linkedinbot.py
+++ linkedinbot.py
@@ -138,6 +138,11 @@ def visibilty(company_name):
             break
 
 
+def start_visibility():
+    links = []
+
+
+
 if __name__ == '__main__':
 
   login_url = "https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin"
